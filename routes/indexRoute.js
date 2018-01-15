1fd6d35a5e7a4656687f53a20e771e93aa7b3a67
Gellert <gellert.li1995@gmail.com>
2018-01-15 21:13:27 +0800

    auto formating

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -25,15 +25,45 @@ router.get('/reception', function(req, res) {
   });
 });
 
-router.get('/testAJAX', function(req, res) {
-  res.render('testAJAX')
-})
+router.get('/kitchen', function(req, res) {
+  var Item = Parse.Object.extend('Item');
+  var query = new Parse.Query(Item);
+  query.find({
+    success: function(results) {
+      console.log('Successfully retrieved ' + results.length + ' items.');
+      var itemMap = new Map();
+      results.forEach(item => {
+        var itemName = item.get('name');
+        if (!itemMap.has(itemName)) {
+          itemMap.set(itemName, item.get('quantity'));
+        } else {
+          itemMap.set(itemName, itemMap.get(itemName) + item.get('quantity'));
+        }
+      });
 
-router.post('/testAJAX', function(req,res) {
-  console.log(req.body);
-  var message = JSON.parse(req.body);
-  res.send(req.body.abc);
-  console.log("string sent");
+      for (var [key, value] of itemMap.entries()) {
+        console.log(key + ' = ' + value);
+      }
+      res.render('kitchen', { items : itemMap });
+    }, 
+    error: function(error) {
+      console.log('Error: ' + error.code + ' ' + error.message);
+    }
+  });
 });
+
+// var globalJSON;
+
+// router.get('/testAJAX', function(req, res) {
+//   res.send(globalJSON.abc);
+// })
+
+// router.post('/testAJAX', function(req,res) {
+//   console.log(req.body);
+//   // res.writeHead(200, {"Content-Type": "text/plain"});
+//   // res.end('qwer');
+//   console.log(req.body.abc);
+//   globalJSON = req.body;
+// });
 
 module.exports = router;
